e06ad112f7bbac6a209d5bab5065a9cea01b1157
jillibus <hughes_0303@man.com>
2021-12-27 12:02:12 -0500

    Upload project files

---
+++ environmentsettings.R
@@ -0,0 +1,3 @@
+library(reticulate)
+use_condaenv(condaenv = "PythonData", conda="/home/jhughes/anaconda3/envs/PythonData")
+use_python("home/jhughes/.local/share/r-miniconda/envs/r-reticulate/bin/python")
